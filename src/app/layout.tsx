606f0ffdec7ff000251fae95d75b07722aba9e40
minyoung13 <mypark0113@naver.com>
2025-12-18 17:49:04 +0900

    feat: Add local fonts and configure global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,12 +10,6 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body>
         {children}
       </body>
